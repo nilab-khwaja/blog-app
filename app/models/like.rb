c185431c4844ddd942ff073d62721542982144db
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-11-29 19:25:10 +0430

    update models for add associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,14 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+
+  after_save :update_like_counter_post
+
+  def author=(user)
+    self.user_id = user.id
+  end
+
+  def update_like_counter_post
+    post.update(like_counter: post.likes.count)
+  end
 end
