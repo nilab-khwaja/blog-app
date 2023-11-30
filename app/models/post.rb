fd1cf281c178661f913967d77865e6ade282b655
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-11-30 20:22:40 +0430

    add validation for the user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   def update_post_counter_user
     author.update(post_counter: author.posts.count)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'comment_id'
   has_many :likes, foreign_key: 'like_id'
 
+  validates :name, presence: true
+  validates :post_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
