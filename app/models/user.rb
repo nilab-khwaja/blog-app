1e33dcb4eb48037b662c0360476401738190ce49
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-12-14 14:17:23 +0430

    modify models to update the like , comment , counter after destory them

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
@@ -18,5 +18,9 @@ class User < ApplicationRecord
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def admin?
+    'admin'
   end
 end
